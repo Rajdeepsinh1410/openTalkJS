38c375f05fcf37127bf4898e1157d61ba608fb51
Rajdeepsinh <dabhirajdipsinh1410@gmail.com>
2025-01-08 11:05:21 +0530

    V0.0.3

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-import ollama from "ollama";
-import fs from "fs";
+import fs from 'fs';
+import ollama from 'ollama';
 
-let q= fs.readFileSync("./q.txt", "utf-8");
-console.log(q)
+let folder = 'questions';
 
-askQuestion()
-async function askQuestion() {
+async function runChat(question) {
   try {
     const response = await ollama.chat({
       model: "llama3.2:latest",
-      messages: [{ role: 'user', content: q }]
+      messages: [{ role: 'user', content: question }]
     });
 
-    fs.writeFileSync("./a.txt", response.message.content);
-
+    return response.message.content;
   } catch (error) {
     console.error("Error occurred:", error.message);
   }
 }
+
+
+fs.readdir(folder, (err, files) => {
+  if (err) {
+    return console.error('Error reading directory:', err.message);
+  }
+
+  files.forEach((file) => {
+    const filepath = `${folder}/${file}`;
+    fs.readFile(filepath, 'utf8', async (err, question) => {
+      if (err) {
+        return console.error(`Error reading file ${file}:, err.message`);
+      }
+      let ans_file = file
+      const response = await runChat(question);
+      console.log(`Question from ${file}:, question`);
+      console.log(`Response:`, response);
+      fs.mkdirSync('answers', { recursive: true })
+      let answer_path = `answers/${ans_file.replace('q','a')}`
+      console.log(`Answer path:`, answer_path)
+      fs.appendFileSync(answer_path, response)
+    });
+  });
+});
